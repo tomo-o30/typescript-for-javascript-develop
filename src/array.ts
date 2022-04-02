5de3ec21bc8f614218026c1ebf2223715c372128
tomo-o30 <tomo.ogawa18@gmail.com>
2022-04-02 18:59:43 +0900

    create array

---
+++ src/array.ts
@@ -0,0 +1,19 @@
+export {};
+
+let numbers: number[] = [1,2,3];
+console.log({numbers});
+
+let numbers2: Array<number> = [1,2,3];
+let string2: Array<string> = ["aaa","iii","uuu"];
+
+
+let strings: string[]= ["aaa","iii","uuu"];
+
+let nizigen: number[][] = [
+  [50, 100],
+  [51, 101]
+];
+
+console.log({nizigen});
+
+let hairetsu: (string | number | boolean)[] = [1, false, "aaa"];
