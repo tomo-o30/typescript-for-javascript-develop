c9b328bbf8db052a55093cacc54f97a00e8f0af1
tomo-o30 <tomo.ogawa18@gmail.com>
2022-04-03 14:13:44 +0900

    create enum

---
+++ src/enum.ts
@@ -0,0 +1,37 @@
+export {};
+
+enum Months {
+  january = 1,
+  febrary,
+  march,
+  april,
+  may,
+  jun,
+  july,
+  august,
+  september,
+  october,
+  november,
+  December
+}
+
+
+console.log(Months.january);
+console.log(Months.febrary);
+
+enum COLORS {
+  RED = "#FF0000",
+  WHITE = "#FFFFFF",
+  GREEN = "#008000",
+  //YELLOW = "#FFF000"
+}
+
+let green = COLORS.GREEN;
+console.log(green);
+
+enum COLORS{
+  YELLOW = "#FFF000",
+  GRAY = "#808080"
+}
+
+COLORS.YELLOW;
