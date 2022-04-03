e7c214d352eeadc1ebb72621128122644df32d5e
tomo-o30 <tomo.ogawa18@gmail.com>
2022-04-03 13:31:56 +0900

    create interface

---
+++ src/interface.ts
@@ -0,0 +1,19 @@
+export {};
+
+
+type ObjectType =  {
+  neme: string;
+  age: number;
+};
+
+interface ObjectInterface {
+  neme: string;
+  age: number;
+}
+
+let object: ObjectInterface = {
+  neme: "ham",
+  age: 43
+}
+
+
